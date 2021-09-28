604cf0bad617430e13ba5af3edf1b98cda9cfbf2
unknown <Pizz@110d0df-ce8ba.38401>
2021-09-28 17:14:29 -0500

    added auth request

---
+++ bin/getAuthToken.js
@@ -0,0 +1,6 @@
+const md5 = require('crypto').createHash('md5');
+let key = '<yoursecret>';
+// timestamp of the expiration time in future
+let exp = (Date.now() / 1000 | 0) + 335445646;
+let streamId = '/live/PizzaPulls';
+console.log(exp+'-'+md5.update(streamId+'-'+exp+'-'+key).digest('hex'));
